70ba6ba2cf0f8a8d46175151ae7649b4ddcbd3ac
Louzada <silvalouzada@gmail.com>
2020-04-07 23:02:51 -0300

    Initial commit

---
+++ wp-content/themes/theme-empty/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title><?php wp_title(); ?></title>
+		<?php wp_head(); ?>
+	</head>
+	<body>
+
+	</body>
+</html>
